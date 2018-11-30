a856facec1e3655379b32b6e1f5a7063e79a4820
LeoCenturion <centurionm.leo@gmail.com>
2018-11-30 01:15:12 -0300

    error puntos cientificos arreglado

--- src/juego/heuristicas.go
+++ src/juego/heuristicas.go
@@ -36,7 +36,7 @@ func (c *Contexto) calcularPeso_puntosDeCartaQueLibera(cartasJugadas [TURNOS]Car
 	return
 }
 
-func (c *Contexto) calcularPeso_promedoProduceDeCartaQueLibera(cartasJugadas [TURNOS]Carta, cartasRestantes [INDICE_MAXIMO_CARTA]Carta, unaCarta Carta) (peso float32) {
+func (c *Contexto) calcularPeso_promedioProduceDeCartaQueLibera(cartasJugadas [TURNOS]Carta, cartasRestantes [INDICE_MAXIMO_CARTA]Carta, unaCarta Carta) (peso float32) {
 
 	if  cartasRestantes[unaCarta.edificioGratis].Id == -1{
 		peso = 0
@@ -46,6 +46,20 @@ func (c *Contexto) calcularPeso_promedoProduceDeCartaQueLibera(cartasJugadas [TU
 	return
 }
 
+func (c *Contexto) calcularPeso_masBarata(cartasJugadas [TURNOS]Carta, cartasRestantes [INDICE_MAXIMO_CARTA]Carta, unaCarta Carta) (peso float32) {
+	peso = float32(0)
+	w := float32(1.0/CANTIDAD_RECURSOS)
+	var pesos [CANTIDAD_RECURSOS]float32
+	for j:=0; j<CANTIDAD_RECURSOS; j++{
+		pesos[j] = w;
+	}
+	for i := 0;i < CANTIDAD_RECURSOS;i++ {
+		peso += float32(1.0/(float32(unaCarta.requiere[i]) * pesos[i]))
+	}
+	
+	return
+}
+
 func (c *Contexto) calcularPeso(cartasJugadas [TURNOS]Carta, cartasRestantes [INDICE_MAXIMO_CARTA]Carta, unaCarta Carta) (peso float32) {
-	return c.calcularPeso_promedioDeProduce(cartasJugadas, cartasRestantes, unaCarta)
+	return c.calcularPeso_puntos(cartasJugadas, cartasRestantes, unaCarta)
 }
